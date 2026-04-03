bcc9cb6099cba46cca48b96cec3e406aa1a80160
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2026-04-03 00:03:13 -0400

    Changes storybook path, installs cva

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,10 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite'
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.@(ts|tsx)'],
+  stories: [
+    '../stories/**/*.stories.@(ts|tsx)',
+    '../components/**/*.stories.@(ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
